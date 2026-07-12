8c96b5c3816d3dfb6a527036cba5a32727ee305b
sobuj54 <sobujahmed124@gmail.com>
2026-07-12 16:34:36 +0600

    update,delete category and TempData notification

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -36,6 +36,72 @@ namespace BulkyBookWeb.Controllers
 
             _context.Categories.Add(category);
             _context.SaveChanges();
+            TempData["success"] = "Category created successfully.";
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Update(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var category = _context.Categories.Find(id);
+            if (category == null) {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Update")]
+        public IActionResult UpdatePost(Category category)
+        {
+            if (!String.IsNullOrEmpty(category.Name) && _context.Categories.Any(c => c.Name.ToLower() == category.Name.ToLower() && c.Id != category.Id))
+            {
+                ModelState.AddModelError("", "Category name already exists.");
+            }
+            if (!ModelState.IsValid) return View();
+
+            _context.Categories.Update(category);
+            _context.SaveChanges();
+            TempData["success"] = "Category updated successfully.";
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var category = _context.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Delete")]
+        public IActionResult DeletePost(int id)
+        {
+            var category = _context.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            _context.Categories.Remove(category);
+            _context.SaveChanges();
+            TempData["success"] = "Category deleted successfully.";
             return RedirectToAction("Index");
         }
     }
